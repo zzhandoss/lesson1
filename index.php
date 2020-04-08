84cbc803af7985c30e9eea46518a461e1b7fff92
zzhandoss <63344632+zzhandoss@users.noreply.github.com>
2020-04-08 14:48:52 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>title</title>
+</head>
+<body>
+	Hello HTML world!!!
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore totam consequatur pariatur minima amet non quaerat, est quis, laboriosam autem, ducimus. Dolores atque expedita ipsum ullam odit eaque similique maxime!</p>
+
+
+
+	Hello PHP world!!!!
+</body>
+</html>
